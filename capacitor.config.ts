a4d8b15df02c8c579b0945a113c495e443882078
Rafael Gomez <rafagomezguillen03@gmail.com>
2023-12-31 13:50:01 +0000

    feat: Creating readme, licence and capacitor config file.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.pet-generator.starter',
   appName: 'Pet Generator',
   webDir: 'dist',
   server: {
