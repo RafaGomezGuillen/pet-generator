11dd3891620dc9fb78bd213359cd363b1e9c571c
Rafael Gomez <rafagomezguillen03@gmail.com>
2024-01-02 10:29:39 +0000

    BREAKING CHANGE: Preparing app to be uploaded to Play Store

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.pet-generator.starter',
+  appId: 'com.petgenerator.app',
   appName: 'Pet Generator',
   webDir: 'dist',
   server: {
