e8ba86342cdbb80e90a784e1d0b7164ca19168a7
RafaGomezGuillen <rafagomezguillen03@gmail.com>
2025-04-02 11:42:38 +0100

    Updates

--- assets/ios.transition-legacy-1iKvnKvJ.js
+++ assets/ios.transition-legacy-1iKvnKvJ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-yh4kH3uW.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-j59LBGY1.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
